79964ec836621ff5fd27e83733542af5c331d95f
Kenny Hung (he) <kenny.cfg@outlook.com>
2023-12-14 19:01:20 +0000

    Square test

---
+++ test_shape.py
@@ -0,0 +1,16 @@
+import unittest
+
+
+class TestShape(unittest.TestCase):
+    def test_square(self):
+        s = Square(6)
+
+        perimeter = s.calc_perimeter()
+        area = s.calc_area()
+
+        self.assertEqual(perimeter, 24)
+        self.assertEqual(area, 36)
+
+
+if __name__ == '__main__':
+    unittest.main()
